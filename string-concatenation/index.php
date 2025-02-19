1f254a1c892cd3ee9517a87ee450e6f6ad763372
dominykam <92014176+dominykam@users.noreply.github.com>
2025-02-19 08:52:19 +0100

    Add solution exercise debug errors

---
+++ string-concatenation/index.php
@@ -0,0 +1,11 @@
+<?php
+    //  first and last name variables
+    $firstName = "Dominyka";
+    $lastName = "Muraskaite";
+
+    // link variables to create full name
+    $fullName = $firstName . " " . $lastName;
+
+    // Get the character count of full name
+    $nameLength = strlen($fullName);
+?>
