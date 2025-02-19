e692bfc0a1e788d98af6a901033ffe020c860a81
dominykam <92014176+dominykam@users.noreply.github.com>
2025-02-19 11:54:42 +0100

    Finished exercise

--- string-functions/index.php
+++ string-functions/index.php
@@ -15,17 +15,14 @@
 
         <?php
         $fruit = "coconut";
-
         $fruitLength = strlen($fruit); //Count how many characters the variable fruit has, using a PHP string function> 
-        
-        echo "Fruit: " . $fruit . "<br>"; //Print this variable>
-
-        echo "Length of Fruit: " . $fruitLength . "<br>"; 
-
         $positionOfO = strpos($fruit, "o");//Determine the position of the first 'o' in the variable <code>$fruit</code>. Print this value.>
-
-        echo "Position of the first 'o': " . $positionOfO;
         ?>
+
+        <p>Fruit: <?= $fruit ?></p>
+        <p> Length of Fruit: <?= $fruitLength ?></p>
+        <p>Position of the first 'o': <?= $positionOfO ?></p>
+        
 
         <h2>Part 2</h2>
 
@@ -34,25 +31,23 @@
         $fruit2Needle = "a";
         $lastPosition = strrpos($fruit2, $fruit2Needle); // Determine the position of the last 'a' in the variable $fruit2
         $uppercaseFruit = strtoupper($fruit2); // Convert the value of $fruit2 to uppercase
-
-        // Output results
-        echo "Last position of 'a' in pineapple: " . $lastPosition . "<br>";
-        echo "Uppercase Fruit: " . $uppercaseFruit . "<br>";
         ?>
 
-        <p> The position of the last '<?= $n?>' is: <?= $lastPosition ?></p>
+        <p> The position of the last '<?= $fruit2Needle ?>' is: <?= $lastPosition ?></p>
+        <p> Uppercase Fruit: <?= $uppercaseFruit ?></p>
 
 
         <h2>Part 3</h2>
 
-        <ul>
-            <li>Create a variable <code>$letter</code> with <code>e</code> as the value.</li>
-            <li>Create a variable <code>$number</code> with <code>3</code> as the value.</li>
-            <li>Create a variable <code>$longestWord</code> with <code>pneumonoultramicroscopicsilicovolcanoconiosis</code> as the value.</li>
-            <li>Now replace all the e's in the <code>$longestWord</code> variable with 3's.
-                <p class="remark">You can only use the variable names. The values <code>e</code>, <code>3</code> and <code>pneumonoultramicroscopicsilicovolcanoconiosis</code> can only appear once in the PHP document.</p>
-            </li>
-        </ul>
+        <?php
+        $letter = "e"; //Create a variable $letter with e as the value.
+        $number = "3";
+        $longestWord = "pneumonoultramicroscopicsilicovolcanoconiosis";
+
+        $changedWord = str_replace($letter, $number, $longestWord); //replace all the e's in the $longestWord variable with 3's.
+        ?>
+
+        <p>Changed word: <?= $changedWord ?></p>
 
     </body>
 </html>
